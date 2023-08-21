cd1009929d375964cb4dca9d0c454e725846d651
mikalai123 <mikalai.kavalionak@gmail.com>
2023-08-21 13:18:08 +0300

    fix: fix code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
+// console.log("nPIBET!!!")
 
-// You should implement your task here.
+module.exports = function towelSort(matrix) {
+  const arr = matrix;
+  if (arr == undefined) {
+    return [];
+  } else {
+    let arr1 = [];
+    const qqq = arr.filter((item, index) => index % 2);
+    for (let index = 0; index < qqq.length; index++) {
+       const element = qqq[index];
+       element.reverse();
+       // console.log(element);
+    }
+   //  console.log(arr);
+    for (let index = 0; index < arr.length; index++) {
+       const element = arr[index];
+       console.log(element);
+       // arr1.push(element);
+       for (let index = 0; index < element.length; index++) {
+           const element1 = element[index];
+           arr1.push(element1);
+       } 
+    }
+   return arr1;
+  }
 
-module.exports = function towelSort (matrix) {
-  return [];
+
+
+
+
+  // let arr1 = [];
+  //  const qqq = arr.filter((item, index) => index % 2);
+  //  for (let index = 0; index < qqq.length; index++) {
+  //     const element = qqq[index];
+  //     element.reverse();
+  //     // console.log(element);
+  //  }
+  // //  console.log(arr);
+  //  for (let index = 0; index < arr.length; index++) {
+  //     const element = arr[index];
+  //     console.log(element);
+  //     // arr1.push(element);
+  //     for (let index = 0; index < element.length; index++) {
+  //         const element1 = element[index];
+  //         arr1.push(element1);
+  //     } 
+  //  }
+  // return arr1;
 }
